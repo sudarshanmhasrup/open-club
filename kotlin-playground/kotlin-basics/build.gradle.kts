620c3f22f0011369c2ab6d688d236aa4228a2f35
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-24 22:57:49 +0530

    Define JVM target for source code to JVM 11

--- kotlin-playground/kotlin-basics/build.gradle.kts
+++ kotlin-playground/kotlin-basics/build.gradle.kts
@@ -1,5 +1,16 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.compiler)
+}
+
+kotlin.compilerOptions {
+    jvmTarget = JvmTarget.JVM_11
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
 }
 
 // Custom build directory
