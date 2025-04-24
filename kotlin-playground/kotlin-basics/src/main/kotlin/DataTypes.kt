45cab622bfc40468e0614ecfe04c2c6f9cff6385
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-24 22:52:11 +0530

    Create DataTypes.kt

---
+++ kotlin-playground/kotlin-basics/src/main/kotlin/DataTypes.kt
@@ -0,0 +1,24 @@
+// Data Types in Kotlin
+fun main() {
+
+    // String data type
+    val firstName = "John"
+    val secondName = "Doe"
+
+    // Integer data type
+    val age = 20
+
+    // Double data type
+    val freelanceHourlyRate = 77.80
+
+    // Boolean data type
+    val isMarried = false
+
+    // Float data type
+    val cgpa = 8.9f
+
+    // Character data type
+    val grade = 'A'
+
+    // Output
+}
